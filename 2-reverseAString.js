f1c4fb2c88f5cdef84fd8d2f09b082ee77b88e65
Habib Afolabi <habibwritescode@gmail.com>
2020-02-03 10:44:59 +0100

    Wrap comments for better readability

--- 2-reverseAString.js
+++ 2-reverseAString.js
@@ -18,8 +18,16 @@ function reverseString(str) {
 
 reverseString("hello");
 
-/* This stumped me for a while. I tried to follow the instruction and turned the string into an array, but I couldn't iterate through each letter since the array turned the string into a single array item. Then I realised I didn't have to follow the suggestion if I could come up with my own solution.*/
+// This stumped me for a while. 
+// I tried to follow the instruction and turned the string into an array.
+// But I couldn't iterate through each letter since the array turned the string into a single array item. 
+// Then I realised I didn't have to follow the suggestion if I could come up with my own solution.
 
-/*I knew I had to iterate through the string starting from the last letter, and that was easy to get with the for loop. I also knew I had to concatenate each letter starting from the last one, so i did str[i] += str[i], but it didn't turn out like I expected. */
+// I knew I had to iterate through the string starting from the last letter, and that was easy to get with the for loop. 
+// I also knew I had to concatenate each letter starting from the last one, so i did str[i] += str[i].
+// But it didn't turn out like I expected.
 
-/*After some time of trying to see what was wrong and not getting it, I decided to use Read-Search-Ask. Read the Hint(not the solution) and realised I needed to create a new string, initialize it to a blank string, then use it to concatenate the characters returned from the for loop. Everything fell into place after that. */
+// After some time of trying to see what was wrong and not getting it.
+// I decided to use Read-Search-Ask. 
+// Read the Hint(not the solution) and realised I needed to create a new string and initialize it to a blank string. 
+// Then use it to concatenate the characters returned from the for loop. Everything fell into place after that.
