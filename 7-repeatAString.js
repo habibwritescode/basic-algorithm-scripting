01e4871243cd97865f829198ff42798c6745c5ab
Habib Afolabi <habibwritescode@gmail.com>
2020-02-04 13:49:09 +0100

    Add explanations to code

--- 7-repeatAString.js
+++ 7-repeatAString.js
@@ -11,14 +11,18 @@
 function repeatStringNumTimes(str, num) {
     // repeat after me
     // my code
+    // declare and initialize an empty string to concatenate repetitions of str with
     let newStr = '';
+    // use a for loop to repeat str for every increment of i when i is at least 1
     for (let i = 1; i <= num; i++) {
+        // checks if num is a positive number i.e 1 or greater than 1
         if (num >= i) {
             newStr += str;
         }
     }
-    // my code
+    // returns an empty string if num is not a positive number i.e zero or less than zero
     return newStr;
+    // my code
 }
 
 repeatStringNumTimes("abc", 5);
