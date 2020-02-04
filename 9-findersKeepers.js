2217a03b1f62202ddb8494be8fec0a1400b09274
Habib Afolabi <habibwritescode@gmail.com>
2020-02-04 18:15:11 +0100

    Add solution using a for loop

--- 9-findersKeepers.js
+++ 9-findersKeepers.js
@@ -9,7 +9,25 @@
 // SOLUTIONS
 
 function findElement(arr, func) {
-    return arr.find(func);
+
+    // return arr.find(func);
+    
+    // The function func is called for all elements of the array arr, one after another
+    // If it returns true, the search is stopped, the item is returned. If nothing found, undefined is returned.
+    
+    // OR
+
+    // loop through the array
+    for (let i = 0; i < arr.length; i++) {
+        // pass each element of the array into the function func and check if it returns true
+        if (func(arr[i])) {
+            // return the element of the array that resolves the above to true
+            return arr[i]
+        }
+    }
+    // return undefined if there is no element of the array that makes the function true
+    // this doesn't have to be included, but added it for readability(?)
+    return undefined
 }
 
 findElement([1, 2, 3, 4], num => num % 2 === 0);
@@ -18,5 +36,6 @@ findElement([1, 2, 3, 4], num => num % 2 === 0);
 // Came across the find() array method in javascript.info
 // This challenge didn't say not to use a method, so :)
 
-// The function func is called for all elements of the array arr, one after another
-// If it returns true, the search is stopped, the item is returned. If nothing found, undefined is returned.
+// EDIT
+
+// Wrote another solution using a for loop just because I can.
