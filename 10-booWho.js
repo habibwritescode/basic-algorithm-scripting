c1e4a044bb37cf9603f80716195fd7309bd367f7
Habib Afolabi <habibwritescode@gmail.com>
2020-02-04 18:36:57 +0100

    Add solution for Boo Who

---
+++ 10-booWho.js
@@ -0,0 +1,24 @@
+// INSTRUCTIONS
+
+// Boo who
+
+// Check if a value is classified as a boolean primitive. Return true or false.
+// Boolean primitives are true and false.
+
+
+// SOLUTION
+
+function booWho(bool) {
+    // What is the new fad diet for ghost developers? The Boolean.
+    // my code
+    // Since boolean primitives are true and false, we use an if statement to check if bool is either, and return true or false as appropriate
+    if (bool === true || bool === false) {
+        return true;
+    }
+    return false
+    // my code
+}
+
+booWho(null);
+
+// Straightforward, I think.
